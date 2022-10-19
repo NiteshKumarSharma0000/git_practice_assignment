fccfcf444ea395802eadf8b7be772877b49036b8
Nitesh Sharma <niteshsharma@Niteshs-MacBook-Pro.local>
2022-10-19 13:36:52 +0530

    pallindrome correction

--- palindrome.js
+++ palindrome.js
@@ -2,12 +2,12 @@ let str="naman";
 let bag1="";
 let bag2="";
 for(let i=0;i<=str.length-1;i++){
-   bag+=str[i];
+   bag1+=str[i];
 }for(let i=str.lenght-1;i>=0;i--){
-   bag+=str[i];
+   bag2+=str[i];
 }if(bag1==bag2){
-console.log("not palindrome");
+console.log(" palindrome");
 
 }else{
- console.log("palindrome");
+ console.log("not palindrome");
 }
